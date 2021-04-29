aa66755299b9688e40c395d771028966e83e9b94
sovietbehemoth <73245562+sovietbehemoth@users.noreply.github.com>
2021-04-28 21:10:56 -0400

    Update preprocessor.ts

--- preprocessor/preprocessor.ts
+++ preprocessor/preprocessor.ts
@@ -1,15 +1,17 @@
-import { FUNCTION_DEF_STACK } from "../lexer.ts";
+import { FUNCTION_DEF_STACK, __DEBUG__ } from "../lexer.ts";
 import { read_file_prep } from "./imports.ts";
 import { types } from "../def.ts";
 
 let ESSENTIAL_DOCUMENT:boolean = false;
 
-function declaration_prep(content: string) {
+//#declare preprocessor
+async function declaration_prep(content: string) {
     const cont:string[] = content.split(" ");
     const retval:any = cont[1];
     if (content.includes("(") && content.includes(")")) {
         const funcname:string = cont[2].split("(")[0].trim();
         const params:Array<any> = content.substring(content.indexOf("(")+1, content.lastIndexOf(")")).split(",");
+        //console.log([funcname, params, retval, []])
         FUNCTION_DEF_STACK.push([funcname, params, retval, []]);
     } else {
         if (cont[1] === "essential") {
@@ -18,14 +20,15 @@ function declaration_prep(content: string) {
     }
 }
 
-function preprocessor_linker(data: string): void {
+//parse preprocessors
+async function preprocessor_linker(data: string) {
     const prep:string = data.split(" ")[0].split("#")[1];
     switch(prep) {
         case "declare":
-            declaration_prep(data);
+            await declaration_prep(data);
             break;
         case "import":
-            read_file_prep(data);
+            await read_file_prep(data);
             break;
     }
 }
