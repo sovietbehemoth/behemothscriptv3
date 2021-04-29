90198edf95f98f2bd9f008a2538010028f01ff45
sovietbehemoth <73245562+sovietbehemoth@users.noreply.github.com>
2021-04-28 21:06:29 -0400

    Update inttype.ts

--- variables/inttype.ts
+++ variables/inttype.ts
@@ -1,6 +1,7 @@
-import { INTTYPE_DEF_STACK } from "../lexer.ts";
+import { INTTYPE_DEF_STACK, __DEBUG__ } from "../lexer.ts";
 import { scan_var_name } from "./strtype.ts";
 
+//determine if string is a number
 function checkstr(content:any): boolean {
     let ret:boolean = true;
     for (let i=0;i<content.length;i++) {
@@ -23,19 +24,22 @@ function checkstr(content:any): boolean {
     else return false;
 }
 
+//**Parse logical equations */
 function eqparse(content:string): number {
     let mainstr:string[] = [];
     let opprotocol:string[] = [];
     let numliteralarr:Array<number> = [];
     let sfromstack:boolean = false;
+
+    //identify operators
     for (let i=0;i<content.length;i++) {
         if (content[i] === "+" || content[i] === "*" || content[i] === "-" || content[i] === "/") {
             mainstr.push(",");
             opprotocol.push(content[i]);
         } else if (content[i] != " ") (mainstr.push(content[i]));
-        //mainstr = mainstr.join("").split(",");
     }
     mainstr = mainstr.join("").split(",")
+    //check for references
     for (let i=0;i<mainstr.length;i++) {
         if (checkstr(mainstr[i]) === false) {
             for (let i2=0;i2<INTTYPE_DEF_STACK.length;i2++) {
@@ -51,6 +55,7 @@ function eqparse(content:string): number {
 
     let basenum:number = numliteralarr[0];
 
+    //Perform equation 
     for (let i=0;i<opprotocol.length;i++) {
         if (opprotocol[i] === "+") basenum = basenum + numliteralarr[i+1];
         else if (opprotocol[i] === "-") basenum = basenum - numliteralarr[i+1];
@@ -60,16 +65,18 @@ function eqparse(content:string): number {
     return basenum;
 }
 
+//**Decode the definition of integer */
 function intparse(content:string) {
     if (checkstr(content) === true) {
-        console.log("s")
         return parseInt(content);
     } else if (content.includes("+") || content.includes("-") || content.includes("/") || content.includes("*")) {
         eqparse(content);
         return eqparse(content);
     }
+    if (__DEBUG__ === true) console.log(`Decoded definition of integer`);
 }
 
+//append integer to stack
 function int_init(content:string): void {
     let pass:string[] = [];
     let name:string[] = [];
@@ -84,8 +91,9 @@ function int_init(content:string): void {
     }
     const intname:string = name.join("").split("int")[1].trim();
     const exec = intparse(pass.join("").trim());
-    console.log(`${intname}: ${exec}`);
+    //console.log(`${intname}: ${exec}`);
     INTTYPE_DEF_STACK.push([exec, intname]);
+    if (__DEBUG__ === true) console.log(`Appended integer '${intname}' to stack`);
 }
 
-export default int_init;
+export {int_init, checkstr, eqparse};
