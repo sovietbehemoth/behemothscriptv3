17376ee7821e6661495e64157bf754976ad0d692
sovietbehemoth <73245562+sovietbehemoth@users.noreply.github.com>
2021-04-28 21:03:14 -0400

    Update def.ts

--- def.ts
+++ def.ts
@@ -1,9 +1,21 @@
-import { STRTYPE_DEF_STACK } from "./lexer.ts";
+import { STRTYPE_DEF_STACK, FUNCTION_DEF_STACK, INTTYPE_DEF_STACK, __DEBUG__ } from "./lexer.ts";
 
-type types = "string" | "int" | "undefined" | "object" | "void";
+//**Prevent compiler error */
+let glob:any = "";
+
+type types = "string" | "int" | "undefined" | "object" | "void" | "(nontype)";
+
+//**Char type */
+type alpha = "a"|"c"|"d"|"e"|"f"|"g"|"h"|"i"|"j"|"k"|"l"|"m"|"n"|"o"|"p"|"q"|"r"|"s"|"t"|"u"|"v"|"w"|"x"|"y"|"z";
+
+//**Check if char is legal */
+const legalchar = new Boolean(glob.toLowerCase() === "a" || glob.toLowerCase() === "b" || glob.toLowerCase() === "c" || glob.toLowerCase() === "d" || glob.toLowerCase() === "e" || glob.toLowerCase() === "f" || glob.toLowerCase() === "g" || glob.toLowerCase() === "h" || glob.toLowerCase() === "i" || 
+                              glob.toLowerCase() === "j" || glob.toLowerCase() === "k" || glob.toLowerCase() === "l" || glob.toLowerCase() === "m" || glob.toLowerCase() === "n" || glob.toLowerCase() === "o" || glob.toLowerCase() === "p" || glob.toLowerCase() === "q" || glob.toLowerCase() === "r" || 
+                              glob.toLowerCase() === "s" || glob.toLowerCase() === "t" || glob.toLowerCase() === "u" || glob.toLowerCase() === "v" || glob.toLowerCase() === "w" || glob.toLowerCase() === "x" || glob.toLowerCase() === "y" || glob.toLowerCase() === "z");
 
 //**Determine the value of code */
 function decode_value(content:any, expected:types): any {
+    if (__DEBUG__ === true) console.log(`Decoding ${expected}...'`);
     let returnvalue:any;
     let located:boolean = false;
     switch (expected) {
@@ -16,9 +28,29 @@ function decode_value(content:any, expected:types): any {
                 }
             }
             break;
+        case "int":
+            for (let i = 0; i < INTTYPE_DEF_STACK.length; i++) {
+                if (content.trim() === INTTYPE_DEF_STACK[i][1]) {
+                    returnvalue = INTTYPE_DEF_STACK[i][0];
+                    located = true;
+                    break;
+                }
+            }
+            break;
     }
     if (located === false) throw "ParserError: Undefined reference to " + expected;
     return returnvalue;
 }
 
-export { decode_value, types };
+//**Reliably check if string input is a function call */
+function isfunction(content:string): boolean {
+    for (let i = 0; i < content.length; i++) {
+        glob = content[i];
+        if (legalchar !== true && glob === "(") {
+            return true;
+        }
+    }
+    return false;
+}
+
+export { decode_value, types, isfunction };
